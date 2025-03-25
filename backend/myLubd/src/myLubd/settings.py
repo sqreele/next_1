263c6f94bc9c43eb0382526fcdb838f085767610
sqreele <kkanthong@gmail.com>
2025-03-25 20:19:29 +0700

    fixbackup1

--- backend/myLubd/src/myLubd/settings.py
+++ backend/myLubd/src/myLubd/settings.py
@@ -240,3 +240,5 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB in bytes
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': '/root/my_project/backend/myLubd/backups/'}
+DBBACKUP_CLEANUP_KEEP = 7
+MEDIABACKUP_CLEANUP_KEEP = 7
