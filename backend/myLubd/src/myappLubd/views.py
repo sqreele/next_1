d79e053e2d8e75659921488abb213ec34f7d7436
sqreele <kkanthong@gmail.com>
2025-05-06 19:53:46 +0700

    addymf

--- backend/myLubd/src/myappLubd/views.py
+++ backend/myLubd/src/myappLubd/views.py
@@ -10,6 +10,8 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 from django.utils import timezone
 from .models import UserProfile, Property, Room, Topic, Job, Session,PreventiveMaintenance
+
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from .serializers import (
     UserProfileSerializer, PropertySerializer, RoomSerializer, TopicSerializer, JobSerializer,
     UserSerializer  # Added for RegisterView
