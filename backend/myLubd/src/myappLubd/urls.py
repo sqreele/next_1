b48a5070eef13abd7430a947e61124ec72a07a65
sqreele <kkanthong@gmail.com>
2025-05-07 22:32:06 +0700

    addfixedittapiuop

--- backend/myLubd/src/myappLubd/urls.py
+++ backend/myLubd/src/myappLubd/urls.py
@@ -62,7 +62,7 @@ path('api/properties/<str:property_id>/is_preventivemaintenance',
          name='job-preventive-maintenance'),
    
     # Your existing paths...
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
 ]
 
 
